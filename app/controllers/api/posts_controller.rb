3c03bf31bdf845b31f5bb890c07bb416692c077f
Leon Carswell <aleon@Leons-MacBook-Air.local>
2017-11-14 10:14:46 -0500

    setup controllers

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,39 @@
+class Api::PostsController < ApplicationController
+    def index
+        @posts = Venue.find(params[:venue_id]).posts
+        render json: @posts
+    end
+
+    def show
+        @post = Post.find(params[:id])
+        render json: @post
+    end
+
+    def create
+        @venue = Venue.find(params[:venue_id])
+        @post = Post.new(post_params)
+
+        @venue.posts << @post
+        @venue.save!
+
+        render status: :ok
+    end
+
+    def update 
+        @post = Post.find(params[:id])
+        @post.update!(song_params)
+        render json: @post
+    end
+
+    def destroy
+        @post = Post.find(params[:id]).delete
+        render status: :ok
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :event, :content, :image_url, :post_rating)
+    end
+
+end
---
+++ app/controllers/api/venues_controller.rb
@@ -0,0 +1,33 @@
+class Api::VenuesController < ApplicationController
+    def index
+        @venues = Venue.all
+        render json: @venues
+    end
+
+    def create
+        @venue = Venue.create!(venue_params)
+        render json: @venue
+    end
+
+    def show
+        @venue = Venue.find(params[:id])
+        render json: @venue
+    end
+
+    def update
+        @venue = Venue.find(params[:id])
+        @venue.update!(venue_params)
+        render json: @venue
+    end
+
+    def destroy 
+        @venue = Venue.find(params[:id]).delete
+        render status: :ok
+    end
+
+    private
+
+    def venue_params
+        params.require(:venue).permit(:name, :address, :dress_type, :rating, :description)
+    end
+end
