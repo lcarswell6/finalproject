580fc46be0e3263581f7a82da6b860f785886e7a
Leon Carswell <aleon@Leons-MacBook-Air.local>
2017-11-14 16:15:50 -0500

    changes to src-components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import User from './components/User';
+import UserList from './components/UserList';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>HEEELLLEEERRR</h1>
-      </div>
+      <Router>
+        <div className="App">
+
+          <div>
+            <h1>Dressed Yet?</h1>
+          </div>
+
+          <Route exact path="/users" component={UserList} />
+          <Route path="/user/:id" component={User} />
+        </div>
+      </Router>
     );
   }
 }
--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class UserList extends Component {
+    state = {
+        error: '',
+        users: []
+    }
+
+    componentWillMount() {
+        getUsers();
+    }
+
+    getUsers = async () => {
+        try {
+            const res = await axios.get('/api/users');
+            await setState({ users: user.data });
+            return res.data;
+        }
+        catch (err) {
+            console.log(err)
+            await setState({ error: err.message })
+            return err.message
+        }
+    }
+
     render() {
+        if (state.error) {
+            return <div>{state.error}</div>
+        }
         return (
             <div>
-                
+                <h1>All Users</h1>
+                {state.users.map(user => (
+                    <div key={user.id}>
+                        <Link to={`/users/${user.id}`}>user.name</Link>
+                    </div>
+                ))}
             </div>
         );
     }
